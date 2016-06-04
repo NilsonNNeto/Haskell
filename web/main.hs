e5b5272a5a7744f6ddfba0b1045e15a9f4445d80
nilsonneto <nilson_neto94@hotmail.com>
2016-06-04 14:53:38 +0000

    Colocando HTML/CSS na Home

--- web/main.hs
+++ web/main.hs
@@ -217,7 +217,114 @@ getErroR = defaultLayout [whamlet|
 |]
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = defaultLayout $ [whamlet|
+                <header>
+                    <nav class="menu-container">
+                        <div class="logo-container">
+                            <a href="">Logo
+                        <ul id="menu">
+                            <li>
+                                <a href="">Home
+                            <li>
+                                <a href="">Home
+                            <li>
+                                <a href="">Home
+                        <div class="clearfix">
+                        <div class="linha-menu">
+                <section>
+                    <div id="main">
+                        <div class="main-content">
+                            <h1 class="titulo">Seja Bem Vindo
+|] >> toWidget [lucius| 
+    *{box-sizing: border-box;}
+    html, body{
+        margin: 0;
+        padding: 0;
+    }
+    body{
+        background-color: #626266;
+    }
+    .clearfix{
+        display: block;
+        clear:both;
+    }
+    
+    
+    /* ---------------------------- 
+                MENU
+    ---------------------------- */
+    .menu-container{
+        background-color: #020304;
+    }
+    
+    .logo-container{
+        float: left;
+    }
+    .logo-container a{
+        padding: 19px 30px;
+        color: #CC9E61;
+        font-size: 20px;
+        font-family: fantasy;
+        text-decoration: none;
+        display: inline-block;
+    }
+    
+    #menu{
+        margin: 0;
+        float: right;
+        margin-right: 20px;
+    }
+    #menu li{
+        display: inline;
+    }
+    #menu li a{
+        padding: 25px 10px 15px;
+        color: #CC9E61;
+        font-size: 20px;
+        font-family: Arial, sans-serif;
+        text-decoration: none;
+        display: inline-block;
+    }
+    #menu li a:hover{
+        color: #541F14;
+        background-color: #CC9E61;
+        border-radius: 30px 30px 0 0;
+    }
+    
+    .linha-menu{
+        height: 5px;
+        background-color: #CC9E61;
+        box-shadow: 0 1px 10px black;
+    }
+    
+    
+    /* ---------------------------- 
+                MAIN
+    ---------------------------- */
+    
+    #main{
+        /*background-color: white;
+        padding: 10px;
+        border-radius: 20px;*/
+        margin: 30px auto;
+        width: 800px;
+        background-color: white;
+        border: 5px solid #541F14;
+        padding: 30px 50px 50px;
+        border-radius: 20px;
+        color: #541F14;
+    }
+    .main-content{
+        
+    }
+    .titulo{
+        font-family: Arial, sans-serif;
+        text-align: center;
+    }
+    fieldset{
+        padding: 20px;
+    }
+|]
 
 connStr = "dbname=d9pva9v7sc2rm1 host=ec2-54-163-240-97.compute-1.amazonaws.com user=bcgxcouvxtiphy password=2XQS1V-eZsjn7oXURLLvTnh2FK port=5432"
 
