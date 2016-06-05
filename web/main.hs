fa6a0bbb5f260bf4a9802e82bd80fc92d805fd80
gustornado <gustavo_oshiro_3@hotmail.com>
2016-06-05 18:13:32 +0000

    Criação da pasta static com css dentro e definição de referência externa de getUsuarioR

--- web/main.hs
+++ web/main.hs
@@ -3,11 +3,13 @@
             MultiParamTypeClasses, DeriveDataTypeable,
             GeneralizedNewtypeDeriving, ViewPatterns, EmptyDataDecls #-}
 import Yesod
+import Yesod.Static
 import Database.Persist.Postgresql
+import Text.Lucius (luciusFile)
 import Data.Text
 import Control.Monad.Logger (runStdoutLoggingT)
 
-data Pagina = Pagina{connPool :: ConnectionPool}
+data Pagina = Pagina{getStatic :: Static, connPool :: ConnectionPool}
 
 instance Yesod Pagina
 
@@ -20,11 +22,16 @@ Usuario json
    deriving Show
 |]
 
+staticFiles "../static" 
+
 mkYesod "Pagina" [parseRoutes|
+
 / HomeR GET
 /usuario/cadastro UsuarioR GET POST
 /usuario/checar/#UsuarioId ChecarUsuarioR GET
 /erro ErroR GET
+/static StaticR Static getStatic
+
 |]
 
 instance YesodPersist Pagina where
@@ -49,150 +56,10 @@ formUsuario = renderDivs $ Usuario <$>
 getUsuarioR :: Handler Html
 getUsuarioR = do
             (widget, enctype) <- generateFormPost formUsuario
-            defaultLayout $ [whamlet|
-                <header>
-                    <nav class="menu-container">
-                        <div class="logo-container">
-                            <a href="">Logo
-                        <ul id="menu">
-                            <li>
-                                <a href="">Home
-                            <li>
-                                <a href="">Home
-                            <li>
-                                <a href="">Home
-                        <div class="clearfix">
-                        <div class="linha-menu">
-                <section>
-                    <div id="main">
-                        <div class="main-content">
-                            <h1 class="titulo">Cadastro de Usuário
-                            <fieldset>
-                                <legend>Preencha o formulário
-                                <form method=post id="form-usuario" enctype=#{enctype} action=@{UsuarioR}>
-                                    ^{widget}
-                                    <input type="submit" value="Salvar" class="submit">
-                                    <span class="clearfix">
-|] >> toWidget [lucius| 
-    *{box-sizing: border-box;}
-    html, body{
-        margin: 0;
-        padding: 0;
-    }
-    body{
-        background-color: #626266;
-    }
-    .clearfix{
-        display: block;
-        clear:both;
-    }
-    
-    
-    /* ---------------------------- 
-                MENU
-    ---------------------------- */
-    .menu-container{
-        background-color: #020304;
-    }
-    
-    .logo-container{
-        float: left;
-    }
-    .logo-container a{
-        padding: 19px 30px;
-        color: #CC9E61;
-        font-size: 20px;
-        font-family: fantasy;
-        text-decoration: none;
-        display: inline-block;
-    }
-    
-    #menu{
-        margin: 0;
-        float: right;
-        margin-right: 20px;
-    }
-    #menu li{
-        display: inline;
-    }
-    #menu li a{
-        padding: 25px 10px 15px;
-        color: #CC9E61;
-        font-size: 20px;
-        font-family: Arial, sans-serif;
-        text-decoration: none;
-        display: inline-block;
-    }
-    #menu li a:hover{
-        color: #541F14;
-        background-color: #CC9E61;
-        border-radius: 30px 30px 0 0;
-    }
-    
-    .linha-menu{
-        height: 5px;
-        background-color: #CC9E61;
-        box-shadow: 0 1px 10px black;
-    }
-    
-    
-    /* ---------------------------- 
-                MAIN
-    ---------------------------- */
-    
-    #main{
-        /*background-color: white;
-        padding: 10px;
-        border-radius: 20px;*/
-        margin: 30px auto;
-        width: 800px;
-        background-color: white;
-        border: 5px solid #541F14;
-        padding: 30px 50px 50px;
-        border-radius: 20px;
-        color: #541F14;
-    }
-    .main-content{
-        
-    }
-    .titulo{
-        font-family: Arial, sans-serif;
-        text-align: center;
-    }
-    fieldset{
-        padding: 20px;
-    }
-    form > div{
-        margin-bottom: 20px;
-    }
-    form label{
-        display: inline-block;
-        width: 100px;
-        height: 20px;
-        line-height: 20px;
-        text-align: right;
-        font-family: Arial, sans-serif;
-    }
-    form input[type=text],
-    form input[type=password],
-    form input[type=email],
-    form select{
-        border-radius:5px;
-        height: 20px;
-        width: 300px;
-    }
-    .submit{
-        background-color: #541F14;
-        padding: 10px 20px;
-        font-size: 15px;
-        border: none;
-        border-radius: 10px;
-        box-shadow: 2px 2px 10px #999;
-        cursor: pointer;
-        color: white;
-        float: right;
-    }
-|]
+            defaultLayout $ do
+                -- toWidget $(luciusFile "templates/lucius/menu.lucius")
+                addStylesheet $ StaticR menu_css
+                $(whamletFile "templates/hamlet/menu.hamlet")
 
 postUsuarioR :: Handler Html
 postUsuarioR = do
@@ -435,4 +302,5 @@ connStr = "dbname=d9pva9v7sc2rm1 host=ec2-54-163-240-97.compute-1.amazonaws.com 
 main::IO()
 main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do 
        runSqlPersistMPool (runMigration migrateAll) pool
-       warp 8080 (Pagina pool)
+       t@(Static settings) <- static "static"
+       warp 8080 (Pagina t pool)
